a6e19d1795cdf55b036f40cbae83eb6d3fc8deb5
Luc Van Minh <luk.mink@gmail.com>
2024-06-07 08:22:53 +0700

    update portfolio

--- dags/config-env_dag.py
+++ dags/config-env_dag.py
@@ -1,38 +1,24 @@
-import json
+from airflow import DAG
 from datetime import datetime, timedelta
-
-from airflow.decorators import dag, task # DAG and task decorators for interfacing with the TaskFlow API
-from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
-
-@dag(
-    # This defines how often your DAG will run, or the schedule by which your DAG runs. In this case, this DAG
-    # will run every 30 mins
-    # schedule_interval=timedelta(minutes=30),
-    # This DAG is set to run for the first time on January 1, 2021. Best practice is to use a static
-    # start_date. Subsequent DAG runs are instantiated based on scheduler_interval
-    # start_date=datetime(2021, 1, 1),
-    # When catchup=False, your DAG will only run for the latest schedule_interval. In this case, this means
-    # that tasks will not be run between January 1, 2021 and 30 mins ago. When turned on, this DAG's first
-    # run will be for the next 30 mins, per the schedule_interval
-    catchup=False,
-    tags=['xxx']) # If set, this tag is shown in the DAG view of the Airflow UI
-def config_env_dag():
-    """
-    ### Config Env Dag
-    """
-
-    @task.bash()
-    def pip_install():
-        """
-        #### Pip install task
-        A simple "pip install" task to install required packages.
-        """
-        # run pip install in /opt/airflow/dags/app/sync
-        return "pip install -r /opt/airflow/dags/app/sync/requirements.txt"
-
-    order_data = pip_install()
+from airflow.operators.docker_operator import DockerOperator
 
 
-    
-example_dag_basic = config_env_dag()
+default_args = {
+    'owner'                 : 'Mostafa Ghadimi',
+    'description'           : 'Use of the DockerOperator',
+    'depend_on_past'        : False,
+    'start_date'            : datetime(2023, 4, 4),
+    'email_on_failure'      : False,
+    'email_on_retry'        : False,
+    'retries'               : 1,
+    'retry_delay'           : timedelta(minutes=5)
+}
+
+with DAG('docker_dag_sample', default_args=default_args, schedule_interval="5 10 * * *", catchup=False) as dag:
+    t1 = BashOperator(
+        task_id='print_hello',
+        bash_command='echo "hello world"'
+    )
+
+t1
